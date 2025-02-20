ba6d23707d64fa0bae273ff53e3b1f285dae2462
ppllama <neelapanchakan@gmail.com>
2025-02-20 14:51:30 +0530

    The Project Begins! Added main.py and constants.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        print("foo")
+
+if __name__ == "__main__":
+    main()
