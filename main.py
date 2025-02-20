4459fbe9c10523ea285738c4559ba8c18f0a914a
ppllama <neelapanchakan@gmail.com>
2025-02-20 17:55:19 +0530

    bug fix main.py

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ from constants import *
 def main():
     pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   
     while True:
-        # print("foo")
+    # Add this event loop to process system events
+        for event in pygame.event.get():
+            pass  # do nothing with events, but process them
+        
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+    # while True:
+    #     screen.fill((0,0,0))
+    #     pygame.display.flip()
+    #     # print("foo")
 
 if __name__ == "__main__":
     main()
